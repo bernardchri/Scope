a7ad7033cafe2d74f947452ba14a462201d535bd
bernardchri <bernardchri@gmail.com>
2026-01-31 21:16:43 +0100

    s1.2 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: 32 }}>
-      <h1>Project Builder</h1>
+      <h1>Project Builder e</h1>
       <p>Setup OK</p>
     </main>
   );
